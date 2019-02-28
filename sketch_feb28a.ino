972a787331d694cd01303e5f3f1580c680a04d88
njcanelito <47820402+njcanelito@users.noreply.github.com>
2019-02-28 08:54:56 -0500

    Add files via upload

---
+++ sketch_feb28a.ino
@@ -0,0 +1,27 @@
+#include <FastLED.h>
+#define NUM_LEDS 135
+#define DATA_PIN 11
+
+CRGB leds[NUM_LEDS];
+
+void setup() {
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);
+}
+void loop() {
+  for(int i=0;i<NUM_LEDS;i++){
+    leds[i] = CRGB::Green;
+    leds[(NUM_LEDS-i)] = CRGB::Red;
+    FastLED.show();
+    delay(30);
+    leds[i] = CRGB::Green;
+    leds[(NUM_LEDS-i)] = CRGB::Red;
+  }
+    for(int i=0;i<NUM_LEDS;i++){
+    leds[i] = CRGB::Blue;
+    leds[(NUM_LEDS-i)] = CRGB::Red;
+    FastLED.show();
+    delay(30);
+    leds[i] = CRGB::Blue;
+    leds[(NUM_LEDS-i)] = CRGB::Red;
+}
+}
